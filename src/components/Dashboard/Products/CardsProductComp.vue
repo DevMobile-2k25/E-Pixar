7e0ba6b69e9acfd48b716ec06eb67f4345784cf0
AnthonyLoche <anthonylocheifc@gmail.com>
2025-04-22 21:51:29 -0300

    FEAT: Adding data

--- src/components/Dashboard/Products/CardsProductComp.vue
+++ src/components/Dashboard/Products/CardsProductComp.vue
@@ -67,7 +67,7 @@ span{
 
 .photo img {
     width: 100%;
-    height: 100%;
+    height: 200px;
     object-fit: cover;
 }
 
--- src/data/products.js
+++ src/data/products.js
@@ -6,70 +6,267 @@ import camisa_jessie from '@/data/images/products/camisa_jessie.jpeg'
 import camisa_mike from '@/data/images/products/camisa_mike.jpeg'
 import camisa_sully from '@/data/images/products/camisa_sully.jpeg'
 import camisa_boo from '@/data/images/products/camisa_boo.jpeg'
+import tiara_rato1 from '@/data/images/products/tiara_rato1.jpeg'
+import tiara_rato2 from '@/data/images/products/tiara_rato2.jpeg'
+import bone_mqueen_beje from '@/data/images/products/bone_mqueen_beje.jpeg'
+import bone_mqueen_blue from '@/data/images/products/bone_mqueen_blue.jpeg'
+import bone_mqueen_red from '@/data/images/products/bone_mqueen_red.jpeg'
+import tiara_toy_buzz from '@/data/images/products/tiara_toy_buzz.jpeg'
+import tiara_toy_woody from '@/data/images/products/tiara_toy_woody.jpeg'
+import tiara_toy_alien from '@/data/images/products/tiara_toy_alien.jpeg'
+import meia_toy_woody from '@/data/images/products/meia_toy_woody.jpeg'
+import meia_toy_buzz from '@/data/images/products/meia_toy_buzz.jpeg'
+import meia_toy_random from '@/data/images/products/meia_toy_random.jpeg'
+import meia_toy_alien from '@/data/images/products/meia_toy_alien.jpeg'
+
+import bone_sa_uni from '@/data/images/products/bone_sa_uni.jpeg'
+import bone_sa_mike from '@/data/images/products/bone_sa_mike.jpeg'
+import bone_sa_sulley from '@/data/images/products/bone_sa_sulley.jpeg'
+
+import camisa_wall_white from '@/data/images/products/camisa_wall_white.jpeg'
+import camisa_wall_greenlime from '@/data/images/products/camisa_wall_greenlime.jpeg'
+import camisa_wall_blue from '@/data/images/products/camisa_wall_blue.jpeg'
+import camisa_wall_yellow from '@/data/images/products/camisa_wall_yellow.jpeg'
+import camisa_wall_green from '@/data/images/products/camisa_wall_green.jpeg'
+
+import meia_diver_ans from '@/data/images/products/meia_diver_ans.jpeg'
+import meia_diver_tris from '@/data/images/products/meia_diver_tris.jpeg'
+import meia_diver_nojo from '@/data/images/products/meia_diver_nojo.jpeg'
+import meia_diver_alegria from '@/data/images/products/meia_diver_alegria.jpeg'
+
 
 export const products = [
-    {
-        id: 1,
-        name: 'Camiseta Manga Longa Monstros S.a, Monsters As, Pixar',
-        price: '100',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        cover: camisa_sa_preta,
-        type: 't-shirt',
-        variants: [
-            {
-                color: 'black',
-                image: camisa_sa_preta,
-            },
-            {
-                color: 'white',
-                image: camisa_sa_branca,
-            },
-        ],
-    },
+  {
+    id: 1,
+    name: 'Camiseta Manga Longa Monstros S.a, Monsters As, Pixar',
+    price: '100',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    cover: camisa_sa_preta,
+    type: 't-shirt',
+    variants: [
+      {
+        color: 'black',
+        image: camisa_sa_preta,
+      },
+      {
+        color: 'white',
+        image: camisa_sa_branca,
+      },
+    ],
+  },
 
-    {
-        id: 2,
-        name: 'Camisa Camiseta Dryfit Premium Desenho Toy Story Personagem Xerife Woody Jessie Vaqueira buzz lightyear',
-        price: '100',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        cover: camisa_jessie,
-        type: 't-shirt',
-        variants: [
-            {
-                color: 'white',
-                image: camisa_jessie,
-            },
-            {
-                color: 'brown',
-                image: camisa_woody,
-            },
-            {
-                color: 'green',
-                image: camisa_buzz,
-            },
-        ],
-    },
+  {
+    id: 2,
+    name: 'Camisa Camiseta Dryfit Premium Desenho Toy Story Personagem Xerife Woody Jessie Vaqueira buzz lightyear',
+    price: '100',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    cover: camisa_jessie,
+    type: 't-shirt',
+    variants: [
+      {
+        color: 'white',
+        image: camisa_jessie,
+      },
+      {
+        color: 'brown',
+        image: camisa_woody,
+      },
+      {
+        color: 'green',
+        image: camisa_buzz,
+      },
+    ],
+  },
 
-    {
-        id: 3,
-        name: 'Camiseta Personagens Monstros SA Parte 1',
-        price: '100',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        cover: camisa_boo,
-        type: 't-shirt',
-        variants: [
-            {
-                color: 'pink',
-                image: camisa_boo,
-            },
-            {
-                color: 'blue',
-                image: camisa_sully,
-            },
-            {
-                color: 'green',
-                image: camisa_mike,
-            },
-        ],
-    },
+  {
+    id: 3,
+    name: 'Camiseta Personagens Monstros SA Parte 1',
+    price: '100',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    cover: camisa_boo,
+    type: 't-shirt',
+    variants: [
+      {
+        color: 'pink',
+        image: camisa_boo,
+      },
+      {
+        color: 'blue',
+        image: camisa_sully,
+      },
+      {
+        color: 'green',
+        image: camisa_mike,
+      },
+    ],
+  },
+
+  {
+    id: 4,
+    name: 'Tiara Ratatoullie adulto',
+    price: '100',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    cover: tiara_rato1,
+    type: 'aceessory',
+    variants: [
+      {
+        color: 'blue',
+        image: tiara_rato1,
+      },
+      {
+        color: 'orange',
+        image: tiara_rato2,
+      },
+    ],
+  },
+
+  {
+    id: 5,
+    name: 'Boné Rust Eze Carros Excelente Qualidade',
+    price: '100',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    cover: bone_mqueen_red,
+    type: 'head',
+    variants: [
+      {
+        color: 'red',
+        image: bone_mqueen_red,
+      },
+      {
+        color: 'blue',
+        image: bone_mqueen_blue,
+      },
+      {
+        color: 'beje',
+        image: bone_mqueen_beje,
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: 'Tiara de orelha para adultos - Toy Story',
+    price: '100',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    cover: tiara_toy_buzz,
+    type: 'acessory',
+    variants: [
+      {
+        color: 'blue',
+        image: tiara_toy_buzz,
+      },
+      {
+        color: 'brown',
+        image: tiara_toy_woody,
+      },
+      {
+        color: 'green',
+        image: tiara_toy_alien,
+      },
+    ],
+  },
+  {
+    id: 7,
+    name: 'Meias infantis divertidas Toy Story',
+    price: '100',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    cover: meia_toy_woody,
+    type: 'socks',
+    variants: [
+        {
+            color: 'blue',
+            image: meia_toy_buzz,
+        },
+        {
+            color: 'brown',
+            image: meia_toy_woody,
+        },
+        {
+            color: 'green',
+            image: meia_toy_alien,
+        },
+        {
+            color: 'yellow',
+            image: meia_toy_random,
+        },
+    ],
+  },
+  {
+    id: 8,
+    name: 'Boné Monstros S.A',
+    price: '100',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    cover: bone_sa_uni,
+    type: 'head',
+    variants: [
+      {
+        color: 'green',
+        image: bone_sa_uni,
+      },
+      {
+        color: 'blue',
+        image: bone_sa_mike,
+      },
+      {
+        color: 'purple',
+        image: bone_sa_sulley,
+      },
+    ],
+  },
+  {
+    id: 9,
+    name: 'Camiseta Manga Longa Wall-E Pixar',
+    price: '100',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    cover: camisa_wall_white,
+    type: 't-shirt',
+    variants: [
+      {
+        color: 'white',
+        image: camisa_wall_white,
+      },
+      {
+        color: 'greenlime',
+        image: camisa_wall_greenlime,
+      },
+      {
+        color: 'blue',
+        image: camisa_wall_blue,
+      },
+      {
+        color: 'yellow',
+        image: camisa_wall_yellow,
+      },
+      {
+        color: 'green',
+        image: camisa_wall_green,
+      },
+    ],
+  },
+  {
+    id: 10,
+    name: 'Meias infantis divertidas Divertidamente',
+    price: '100',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    cover: meia_diver_ans,
+    type: 'socks',
+    variants: [
+        {
+            color: 'blue',
+            image: meia_diver_ans,
+        },
+        {
+            color: 'green',
+            image: meia_diver_alegria,
+        },
+        {
+            color: 'yellow',
+            image: meia_diver_nojo,
+        },
+        {
+            color: 'pink',
+            image: meia_diver_tris,
+        },
+    ],
+  }
+  
 ]
