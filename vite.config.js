66b96d18353c8a4095b80113be1d16426727263e
AnthonyLoche <anthonylocheifc@gmail.com>
2025-04-24 22:48:25 -0300

    Add web app manifest files for MyWebSite and E-Pixar
    - Created site.webmanifest for MyWebSite with icons and display settings.
    - Created manifest.json for E-Pixar with app details and icons.

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,46 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
   plugins: [
     vue(),
     // vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'E-Pixar',
+        short_name: 'E-Pixar',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'], // Garante que encontre arquivos válidos
+        globIgnores: ['sw.js', 'workbox-*.js']
+      }
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  assetsInclude: ['**/*.glb'], // Adicione esta linha aqui
+  assetsInclude: ['**/*.glb'],
 })
