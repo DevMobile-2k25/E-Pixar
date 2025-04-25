2a2765f8333b74dde71fa8b62c89addf4ef0c4a7
AnthonyLoche <anthonylocheifc@gmail.com>
2025-04-24 22:25:33 -0300

    FEAT: Reorganizar estrutura do componente PersonsComp; mover template para a posição correta e ajustar propriedades de estilo

--- src/components/OneBoard/PersonsComp.vue
+++ src/components/OneBoard/PersonsComp.vue
@@ -1,16 +1,9 @@
-<script setup>
-</script>
-
-<template>
-    <img src="../../assets/images/OneBoard/elephant.png" alt="" class="elephant">
-    <img src="../../assets/images/OneBoard/mcqueen.png" alt="" class="mcqueen">
-    <img src="../../assets/images/OneBoard/monster.png" alt="" class="monster">
-    <img src="../../assets/images/OneBoard/remi.png" alt="" class="remi">
-    <img src="../../assets/images/OneBoard/up.png" alt="" class="up">
-    <img src="../../assets/images/OneBoard/wall.png" alt="" class="wall">
-</template>
-
 <style scoped>
+/* Adicione essa regra se não houver uma regra global */
+:global(body) {
+  overflow-x: hidden;
+}
+
 .elephant {
     position: absolute;
     top: 47%;
@@ -21,14 +14,14 @@
 .mcqueen {
     position: absolute;
     top: -7%;
-    right: -5%;
+    right: 0;
     width: 50%;
 }
 
 .monster {
     position: absolute;
     top: 45%;
-    right: -5%;
+    right: 0;
     width: 50%;
 }
 
@@ -53,3 +46,12 @@
     width: 50%;
 }
 </style>
+
+<template>
+  <img src="../../assets/images/OneBoard/elephant.png" alt="" class="elephant">
+  <img src="../../assets/images/OneBoard/mcqueen.png" alt="" class="mcqueen">
+  <img src="../../assets/images/OneBoard/monster.png" alt="" class="monster">
+  <img src="../../assets/images/OneBoard/remi.png" alt="" class="remi">
+  <img src="../../assets/images/OneBoard/up.png" alt="" class="up">
+  <img src="../../assets/images/OneBoard/wall.png" alt="" class="wall">
+</template>
